d457ad26b74010c8a6b5394a40bcb221c8fcddfa
sromero <sgromero2010@gmail.com>
2025-02-10 19:25:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+ document.addEventListener("DOMContentLoaded", function () {
+  lottie.loadAnimation({
+    container: document.getElementById("lottie-logo"),
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+path: './anima.json'
+  });
+});
