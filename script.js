95e58484043173900b70a0d6a418d267086d21b0
sromero <sgromero2010@gmail.com>
2025-02-13 12:32:13 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
- document.addEventListener("DOMContentLoaded", function () {
-  lottie.loadAnimation({
-    container: document.getElementById("lottie-logo"),
-    renderer: "svg",
-    loop: true,
-    autoplay: true,
-path: './anima.json'
-  });
+emailjs.init('hb8EKPFvRVQ4TVStL');
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const submitButton = document.getElementById("submitButton");
+    submitButton.textContent = "Enviando...";
+
+    emailjs.sendForm("service_6g5vs5r", "template_7kt1j7z", this)
+        .then(function(response) {
+            submitButton.textContent = "Enviar Mensaje";
+            document.getElementById('contactForm').reset();
+            alert("Mensaje enviado con éxito!");
+        }, function(error) {
+            submitButton.textContent = "Enviar Mensaje";
+            alert("Hubo un error al enviar el mensaje. Intenta de nuevo.");
+            console.log(error);
+        });
 });
